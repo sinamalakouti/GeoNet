448d9884ba147c15eb075b4ec78a450edf6c5d19
sina malakouti <siinamalakouti@gmail.com>
2024-04-29 09:58:26 -0400

    Update plain.py

--- UDA_trainer/plain.py
+++ UDA_trainer/plain.py
@@ -1,15 +1,14 @@
 import torch
 
-
 def train_plain(batch_iterator, model_fe, model_cls, opt, it, criterion_cls,
-                cfg, logger, writer):
-    device = 'cuda' if torch.cuda.is_available() else "cpu"
+            cfg, logger, writer):
+
     # setting training mode
     opt.zero_grad()
 
     # get data
-    (_, img_src, lbl_src), (_, img_tgt, lbl_trgt) = next(batch_iterator)
-    img_src, lbl_src = img_src.to(device), lbl_src.to(device)
+    (_, img_src, lbl_src), (_, img_tgt,_) = next(batch_iterator)
+    img_src, lbl_src = img_src.cuda(), lbl_src.cuda()
 
     # forward
 
@@ -23,10 +22,10 @@ def train_plain(batch_iterator, model_fe, model_cls, opt, it, criterion_cls,
     curr_lr = opt.param_groups[0]['lr']
     if (it + 1) % cfg['training']['print_interval'] == 0:
         print_str = 'Iteration: [{0}/{1}]\t' \
-                    'LR: [{curr_lr:.4g}]\t' \
-                    'CLoss {closs:.4f}\t'.format(
-            it + 1, cfg['training']['iteration'], curr_lr=curr_lr,
-            closs=loss.item()
-        )
+            'LR: [{curr_lr:.4g}]\t' \
+            'CLoss {closs:.4f}\t'.format(
+                it + 1, cfg['training']['iteration'], curr_lr=curr_lr,
+                closs=loss.item()
+            )
 
         logger.info(print_str)
