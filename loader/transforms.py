e6cb50b201625b83d7103aa1cfdb2554d3359bf1
sina malakouti <siinamalakouti@gmail.com>
2024-05-09 10:19:24 -0400

    update

--- loader/transforms.py
+++ loader/transforms.py
@@ -83,10 +83,9 @@ def test_transform():
     return trans
 
 def clip_transform():
-    def _transform(n_px):
-        return Compose([
-            Resize(n_px, interpolation=BICUBIC),
-            CenterCrop(n_px),
+    return Compose([
+            Resize(224, interpolation=BICUBIC),
+            CenterCrop(224),
             _convert_image_to_rgb,
             ToTensor(),
             Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
